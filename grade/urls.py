c3623a5f781e6b6eb598fcc2dbfeb723ce2bd576
Pattyawat Jungpan <p.jungp@union-button.com>
2025-06-25 17:35:32 +0700

    update git

--- grade/urls.py
+++ grade/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from .views import Grade
 from .views import GradeListCreateAPIView, GradeRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
--- grade/views.py
+++ grade/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from .models import Grade
 from .serializers import GradeSerializer
 
+class SetPaginationGrade(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    page_query_param = 'page' 
+
 class GradeListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Grade.objects.all()
+    queryset = Grade.objects.filter(is_active=1)
     serializer_class = GradeSerializer
 
 class GradeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Grade.objects.all()
     serializer_class = GradeSerializer
     lookup_field = 'grade_id'
+
+    def perform_destroy(self, instance):
+        if instance.is_active:
+            instance.is_active = 0
+            instance.save()
+        else:
+            pass
 
